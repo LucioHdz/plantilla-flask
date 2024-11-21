92354e4bb8819d5c9f9a66d1a5ecc9e074442ae7
LucioHdz <luciohdz0@gmail.com>
2024-11-21 00:51:06 -0600

    clase 1.2

--- app/app.py
+++ app/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+# Creación del servidor
+server = Flask(__name__)
 
-@app.route("/")
-def saludar():
-    return "Hola mundo"
+@server.route("/", methods=["GET"])
+def get_data():
+    """
+    Returns all data from the database
+    """
+    return "returns all data from the database"
+
+@server.route("/<int:id>")
+def get_one_by_id(id):
+    """
+    Returns a data from the database by id
+    """
+    return f"returns a data from the database by id = {id}"
+
+@server.route("/", methods=["POST"])
+def add_data():
+    """
+    Adds new data
+    """
+    return jsonify(request.json), 201
+
+@server.route("/<int:id>", methods=["PATCH", "PUT"])
+def modify_data(id):
+    """
+    Modifies existing data
+    """
+    return jsonify(request.json)
+
+@server.route("/<int:id>", methods=["DELETE"])
+def delete_data(id):
+    """
+    Deletes an item from the database
+    """
+    return f"delete an item from the data"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    server.run(host="0.0.0.0", port=5000, debug=True)
