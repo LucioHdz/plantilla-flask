a354b8bbf3465396f9c0f1628a568c1dc0f83f60
LucioHdz <luciohdz0@gmail.com>
2024-11-20 22:49:08 -0600

    clase 1

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-# import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hola mundo desde flask"
+@app.route("/")
+def saludar():
+    return "Hola mundo"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
